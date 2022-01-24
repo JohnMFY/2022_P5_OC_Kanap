ff11408c4e55249434bff25e8e871a87952e996b
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2022-01-24 23:18:34 +0100

    Product data fully dynamically include on product page

--- front/Javascript/products.js
+++ front/Javascript/products.js
@@ -1,4 +1,4 @@
-// hello HELL //
+/////////* AH AH AH, YOU DIDN'T SAY THE MAGIC WORD */////////
 
 ////// RECOVERY OF THE ID PRODUCT //////
 const productUrlId = window.location.search
@@ -7,46 +7,63 @@ const idProduct= UrlParams.get("id")
 console.log(idProduct)
 
 ////// GET THE PRODUCT DATA WITH THE ID  //////
-const callProductById = async() =>{
+ async function callProductById(){
   await fetch(`http://localhost:3000/api/products/${idProduct}`)
-   .then((res) => res.json())
-   .then((data) => {
-      productData = data
-      console.log(productData)
-   })
+   .then(res => res.json())
+   .then((data) => (productData = data)) 
+   console.log(productData)  
 }
-callProductById()
-let productData = []
+
 ////// DATA TO INSERT //////
 const productDisplay = async () => {
    await callProductById()
 
+   //IMG & ALT//
    document.querySelector('.item__img').innerHTML = `
       <img src="${productData.imageUrl}" alt="${productData.altTxt}">
    `
+   //TITLE//
    document.getElementById('title').innerHTML=`
       <h1 id="title">${productData.name}</h1>
    `
+   //PRICE//
    document.getElementById('price').innerHTML=`
       <span id="price">${productData.price}</span>
    `
-
+   //DESCRIPTION//
    document.getElementById('description').innerHTML=`
       <p id="description">${productData.description}</p>
    `
-   document.getElementById('colors').innerHTML=`
-      <option value="">--SVP, choisissez une couleur --</option>
-         <option value="vert">vert</option>
-         <option value="blanc">blanc</option>
-   `  /*
-         for (let i=0; i < ${productData.colors}.length; i++){
-         document.getElementById('colors').innerHTML += `
-            <option value="">--SVP, choisissez une couleur --</option>
-            <option value="$productData[i].colors">$productData[i].colors</option>
-         `
-      */
+   //COLORS SELECTION//
+   let colors = productData.colors
+   console.log(colors)
+   for (let i=0; i < colors.length; i++){
+      document.getElementById('colors').innerHTML += `
+         <option value="${colors[i]}">${colors[i]}</option>
+      `
+   }    
 }
 productDisplay()
-/*
 
-*/
+/*                                                  ____
+       ___                                      .-~. /_"-._
+      `-._~-.                                  / /_ "~o\  :Y
+          \  \                                / : \~x.  ` ')
+           ]  Y                              /  |  Y< ~-.__j
+          /   !                        _.--~T : l  l<  /.-~
+         /   /                 ____.--~ .   ` l /~\ \<|Y
+        /   /             .-~~"        /| .    ',-~\ \L|
+       /   /             /     .^   \ Y~Y \.^>/l_   "--'
+      /   Y           .-"(  .  l__  j_j l_/ /~_.-~    .
+     Y    l          /    \  )    ~~~." / `/"~ / \.__/l_
+     |     \     _.-"      ~-{__     l  :  l._Z~-.___.--~
+     |      ~---~           /   ~~"---\_  ' __[>
+     l  .                _.^   ___     _>-y~
+      \  \     .      .-~   .-~   ~>--"  /
+       \  ~---"            /     ./  _.-'
+        "-.,_____.,_  _.--~\     _.-~
+                    ~~     (   _}       -Row
+                           `. ~(
+                             )  \
+                            /,`--'~\--'~\
+   https://www.youtube.com/watch?v=RfiQYRn7fBg*/
