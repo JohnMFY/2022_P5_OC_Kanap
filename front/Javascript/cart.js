70c2866a4343d53f6d54a95bb86a02316d648b7a
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2022-02-05 22:39:25 +0100

    incrementation with innerHTML of the products data from the local storage in the dom

--- front/Javascript/cart.js
+++ front/Javascript/cart.js
@@ -1,34 +1,24 @@
-let dataInLocalStorage = JSON.parse(localStorage.getItem('productDataLocalStorage'))
-    console.log(dataInLocalStorage.ProductId)
 
-let idInLocalStorage = dataInLocalStorage
-console.log(idInLocalStorage)
-/*
-async function callProductById(){
-    await fetch(`http://localhost:3000/api/products/${idInLocalStorage}`)
-    .then(res => res.json())
-    .then((data) => (productData = data))  
-}
+function productCartInsertion(){
 
-    async function productCartInsertion(){
-    await callProductById()
     let productCartContainer = document.getElementById('cart__items');
-        
+    let dataInLocalStorage = JSON.parse(localStorage.getItem('productDataLocalStorage'))
+
     for (let i=0; i < dataInLocalStorage.length; i++){
         productCartContainer.innerHTML +=
     `
     <article class="cart__item" data-id="${dataInLocalStorage[i].ProductId}" data-color="${dataInLocalStorage[i].ProductColor}">
 
         <div class="cart__item__img">
-            <img src="${productData[i].imageUrl}">
+            <img src="IMAGE">
         </div>
 
         <div class="cart__item__content">
 
             <div class="cart__item__content__description">
-                <h2>${productData[i].name}</h2>
+                <h2>TITLE</h2>
                 <p>ProductColor : ${dataInLocalStorage[i].ProductColor}</p>
-                <p> ${productData[i].price} €</p>
+                <p> PRICE €</p>
             </div>
 
             <div class="cart__item__content__settings">
@@ -37,7 +27,7 @@ async function callProductById(){
                     <p>Qté : ${dataInLocalStorage[i].ProductQuantity}</p>
                     <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${dataInLocalStorage[i].ProductQuantity}">
                 </div>
-
+                
                 <div class="cart__item__content__settings__delete">
                     <p class="deleteItem">Supprimer</p>
                 </div>
