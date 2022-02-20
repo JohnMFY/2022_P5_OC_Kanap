5d318946ebd7dd302595414b0e22ecd54d9332d9
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2022-02-20 22:43:11 +0100

    total price FIP / modification quantity FIP / POW recuperation data from form

--- front/Javascript/cart.js
+++ front/Javascript/cart.js
@@ -21,15 +21,15 @@ async function looping(){
             productData = data;
             const article = render(productData, productDataLocalStorage);
             return article;
-
+            
         })
 
         .catch((error) => console.log(error)))
-
     }
-  
+
   return articlesPromise;
-}
+}  
+
 //// RENDER PRODUCT ARTICLE ////
 
     const render = (dataAPI, dataLocalStorage) =>{
@@ -69,104 +69,171 @@ async function looping(){
     }
 
 
-
+//// SUPPRESSION MODIFICATION AND CALCUL OF TOTAL PRICE ////
 async function afterPromise() {
     await looping().then(articlesPromesse => {
-
+    
         Promise.all(articlesPromesse) // Recuperation of all promise
 
         .then(articles => {
-                
+
             boxArticleCart.innerHTML = articles;
             
             //// DELETE PRODUCT OF CART ////
 
-            let deleteBtn = Array.from(document.getElementsByClassName('deleteItem')) // create an array of the HTMLcollection
+                let deleteBtn = Array.from(document.getElementsByClassName('deleteItem')) // create an array of the HTMLcollection
 
-            for(let i = 0; i < deleteBtn.length; i++){
+                for(let i = 0; i < deleteBtn.length; i++){
 
-                let buttonDel = deleteBtn[i]
+                    let buttonDel = deleteBtn[i]
 
-                buttonDel.addEventListener('click', function(e){
+                    buttonDel.addEventListener('click', function(e){
 
-                    console.log('click is working')
+                        console.log('click is working')
 
-                    // Remove article of the DOM //
-                    let buttonDelClick = e.target
-                    buttonDelClick.closest('.cart__item').remove()
+                        // Remove article of the DOM //
+                        let buttonDelClick = e.target
+                        buttonDelClick.closest('.cart__item').remove()
 
-                    // Remove item from LocalStorage //
-                   
-                    let deletedId = dataInLocalStorage[i].ProductId;
-                    let deletedColor = dataInLocalStorage[i].ProductColor; // recuperation of data id & color associate to the article of the buttonDel clicked
+                        // Remove item from LocalStorage //
                     
-                    dataInLocalStorage = dataInLocalStorage.filter( e => e.ProductId !== deletedId || e.ProductColor !== deletedColor ); // exclusion of data from upper variable and creation of new array         
-                    localStorage.setItem('productDataLocalStorage', JSON.stringify(dataInLocalStorage)); // replacement of array in LocalStorage
+                        let deletedId = dataInLocalStorage[i].ProductId;
+                        let deletedColor = dataInLocalStorage[i].ProductColor; // recuperation of data id & color associate to the article of the buttonDel clicked
+                        
+                        dataInLocalStorage = dataInLocalStorage.filter( e => e.ProductId !== deletedId || e.ProductColor !== deletedColor ); // exclusion of data from upper variable and creation of new array         
+                        localStorage.setItem('productDataLocalStorage', JSON.stringify(dataInLocalStorage)); // replacement of array in LocalStorage
 
-                    location.reload(); // we can use location.reload() to avoid "bug" (Cannot read properties of undefined (reading 'ProductId')) who can appear time to time with big cart
-                    
-                })       
-            } 
+                        location.reload(); // we can use location.reload() to avoid "bug" (Cannot read properties of undefined (reading 'ProductId')) who can appear time to time with a big cart
+                        
+                    })       
+                } 
 
             //// TOTAL PRICE & QUANTITY ////
             
-                // QUANTITY IN THE DOM //
+                // QUANTITY INTEGRATE IN THE DOM //
 
                     let productsTotalQuantity = 0
                     let productsQuantityInput = Array.from(document.getElementsByClassName('itemQuantity'))
                     
                     for (let j = 0; j < productsQuantityInput.length; j++){
                         productsTotalQuantity += productsQuantityInput[j].valueAsNumber //valueAsNumber if only value = JS dark magic string + string 1+2 = 12
+                        
                     }
 
                     let totalQuantity = document.getElementById('totalQuantity')
                     totalQuantity.innerHTML = productsTotalQuantity
                     
-                //
+                // TOTAL PRICE INTEGRATE IN THE DOM //
 
+                    // /!\ BUG  N'utilise que le dernier price integré dans le DOM /!\
+
+                   let productsTotalPrice = 0
+
+                    for (let j = 0; j < productsQuantityInput.length; j++){
+                        productsTotalPrice += (productsQuantityInput[j].valueAsNumber * productData.price)
+                        
+                        console.log(productsTotalPrice)
+                    }
+                    console.log(productsTotalPrice)
+                    let totalPrice = document.getElementById('totalPrice')
+                    totalPrice.innerHTML = productsTotalPrice
+
+            //// MODIFICATION OF QUANTITY ////
+
+                // /!\ BUG  the quantity of the 1st product change on every change of other product (with the same value of the change done) /!\
+
+                for (let k = 0; k < productsQuantityInput.length; k++){  
+                    
+        
+                    productsQuantityInput[k].addEventListener("change" , (e) => {
+                        
+                        let quantityModif = dataInLocalStorage[k].ProductQuantity
+                       
+                        let productsQuantityInputValue = productsQuantityInput[k].value
+                        
+                        const newQantity = dataInLocalStorage.find((product) => product.productsQuantityInputValue !== quantityModif)
+        
+                        newQantity.ProductQuantity = productsQuantityInputValue
+                        dataInLocalStorage[k].ProductQuantity = newQantity.ProductQuantity
+        
+                        localStorage.setItem('productDataLocalStorage', JSON.stringify(dataInLocalStorage))
+        
+                        location.reload();
+                    })
+                    
+                }
         });
     });        
 }
 
 afterPromise();
-  
+
+//// RECUPERATION OF FORM DATA ////
 
 
-
-    
 /*
-    // PRICE //
-       let productTotalPrice = 0
-        productData.forEach(product => {
-            productTotalPrice += product.price * product.ProductQuantity
-        })
-        console.log(productTotalPrice)
-        const totalPrice = document.getElementsById('totalPrice')
-        totalPrice.innerHTML = `
-        ${productTotalPrice}
-        `
-   
-//// MODIFICATION OF QUANTITY ////
 
-        let quantitySelected = document.getElementsByClassName("itemQuantity");
-      
+  **************
+ * HTML DU FORM *
+  **************
 
-        for (let i = 0; i < quantitySelected.length; i++){  
-            
+div class="cart__price">
 
-            quantitySelected[i].addEventListener("change" , (event) => {
-                
-                let quantityModif = dataInLocalStorage[i].ProductQuantity;
-                let quantitySelectedValue = quantitySelected[i].value;
-                
-                const resultFind = dataInLocalStorage.find((element) => element.quantitySelectedValue !== quantityModif);
+    <p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>
 
-                resultFind.ProductQuantity= quantitySelectedValue;
-                dataInLocalStorage[i].ProductQuantity = resultFind.ProductQuantity;
+</div>
 
-                localStorage.setItem('productDataLocalStorage', JSON.stringify(dataInLocalStorage));
+<div class="cart__order">
 
-                location.reload();
-            })
-        }
+    <form method="get" class="cart__order__form">
+
+        <div class="cart__order__form__question">
+
+            <label for="firstName">Prénom: </label>
+            <input type="text" name="firstName" id="firstName" required>
+            <p id="firstNameErrorMsg"><!-- ci est un message d'erreur --></p>
+
+        </div>
+
+        <div class="cart__order__form__question">
+
+            <label for="lastName">Nom: </label>
+            <input type="text" name="lastName" id="lastName" required>
+            <p id="lastNameErrorMsg"></p>
+
+        </div>
+
+        <div class="cart__order__form__question">
+
+            <label for="address">Adresse: </label>
+            <input type="text" name="address" id="address" required>
+            <p id="addressErrorMsg"></p>
+
+        </div>
+
+        <div class="cart__order__form__question">
+
+            <label for="city">Ville: </label>
+            <input type="text" name="city" id="city" required>
+            <p id="cityErrorMsg"></p>
+
+        </div>
+
+        <div class="cart__order__form__question">
+
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email" required>
+            <p id="emailErrorMsg"></p>
+
+        </div>
+
+        <div class="cart__order__form__submit">
+
+            <input type="submit" value="Commander !" id="order">
+
+        </div>
+        
+    </form>
+
+</div>
 */
